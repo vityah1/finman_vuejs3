7f1fc83dbc25d687381598872b9db02ad77c9eaa
vityah1 <vholoshivskiy@gmail.com>
2024-10-20 01:45:51 +0300

    fix public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/finman/',
+  publicPath: '/',
   productionSourceMap: false  
 })
